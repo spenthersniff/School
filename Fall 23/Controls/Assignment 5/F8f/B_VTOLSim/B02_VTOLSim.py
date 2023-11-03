79d2a27fe4958ddfda7ff072377a103a687ead8b
spenthersniff <spencersmith514@gmail.com>
2023-11-03 12:58:38 -0400

    F8f done

---
+++ Fall 23/Controls/Assignment 5/F8f/B_VTOLSim/B02_VTOLSim.py
@@ -0,0 +1,126 @@
+import sys
+sys.path.append('.')
+sys.path.append('C:/Users/spenc/Documents/School/Fall 23/Controls/Assignment 5/F8f')
+import matplotlib.pyplot as plt
+import numpy as np
+import parameters.VTOLParam as P
+from tools.signalGenerator import signalGenerator
+from viewer.VTOLAnimation import VTOLAnimation
+from viewer.dataPlotter import dataPlotter
+from dynamics.VTOLDynamics import VTOLDynamics
+from controller.VTOLcontroller import controller
+
+# instantiate reference input classes
+reference = signalGenerator(amplitude=0.5, frequency=0.1)
+zRef = signalGenerator(amplitude=0.5, frequency=0.1)
+# fRef = P.f0
+z_signal = signalGenerator(amplitude=2.5, frequency=0.08)
+
+# instantiate the simulation plots and animation
+vtol=VTOLDynamics()
+# dataPlot = dataPlotter()
+animation = VTOLAnimation(limits=10, multi=True)
+control=controller()
+
+# inital values
+z=0
+z_target=3
+u=0
+h = 5.0
+
+# tune controller
+# z
+tr_z = 2.9
+wn_z = 2.2/tr_z
+dr = 0.707
+b = 2*dr*wn_z
+c = wn_z**2
+control.kDz = (b - P.mu/(P.mc + 2*P.mr))/-P.g
+control.kPz = c/-P.g
+# h
+tr_h = 2.0
+wn_h = 2.2/tr_h
+b = 2*dr*wn_h
+c = wn_h**2
+control.kDh = b*(P.mc + 2*P.mr)
+control.kPh = c*(P.mc + 2*P.mr)
+# theta
+tr_th = 0.8 # stays the same, not tuning this
+wn_th = 2.2/tr_th
+b = 2*dr*wn_th
+c = wn_th**2
+control.kDtheta = b*(P.jc + 2*P.mr*P.d**2)
+control.kPtheta = c*(P.jc + 2*P.mr*P.d**2)
+
+
+
+# plotting
+zplt = animation.fig.add_subplot(4, 2, 2)
+zplt.set_ylabel('z Location (m)')
+fplt = animation.fig.add_subplot(4, 2, 4)
+fplt.set_ylabel('Force (N)')
+hplt = animation.fig.add_subplot(4, 2, 6)
+hplt.set_ylabel('Height (m)')
+thetaplt = animation.fig.add_subplot(4, 2, 8)
+thetaplt.set_ylabel('Theta (deg)')
+
+# initialize lists to plot
+simtimes=[]
+frs=[]
+fls=[]
+zs=[]
+hs=[]
+thetas=[]
+z_targets=[]
+h_targets=[]
+
+
+t = P.t_start  # time starts at t_start
+while t < P.t_end:  # main simulation loop
+    t_next_plot = t + P.t_plot
+    while t < t_next_plot:
+        
+        z_target = 3 + z_signal.square(t)
+        h_target=5.0
+    
+        fr, fl=control.update(z_target, h_target, vtol.state)
+        y=vtol.update(fr,fl)
+        t = t + P.Ts
+    animation.update(vtol.state)
+
+    simtimes.append(t)
+    frs.append(fr)
+    fls.append(fl)
+    zs.append(y[0][0])
+    hs.append(y[1][0])
+    thetas.append(y[2][0])
+
+    z_targets.append(z_target) 
+    h_targets.append(h_target) 
+
+    # Plot
+    zplt.clear()
+    zplt.plot(simtimes,zs,label="z Position")
+    zplt.plot(simtimes,z_targets, label="z Target")
+    zplt.set_ylabel("z Position (m)")
+    zplt.legend(loc="upper left")
+    zplt.grid()
+    fplt.clear()
+    fplt.plot(simtimes,frs,label="Fr")
+    fplt.plot(simtimes,fls,label="Fl")
+    fplt.legend(loc="upper left")
+    fplt.set_ylabel("Force (N)")
+    fplt.grid()
+    hplt.clear()
+    hplt.plot(simtimes,hs,label="Height")
+    hplt.plot(simtimes,h_targets, label="h Target")
+    hplt.set_ylabel("Height (m)")
+    hplt.grid()
+    thetaplt.clear()
+    thetaplt.plot(simtimes,np.degrees(thetas))
+    thetaplt.set_xlabel("Time (s)")
+    thetaplt.set_ylabel("Theta (deg)")
+    thetaplt.grid()
+
+    plt.pause(0.01)  # allow time for animation to draw
+plt.close()
---
+++ Fall 23/Controls/Assignment 5/F8f/viewer/VTOLAnimation.py
@@ -0,0 +1,103 @@
+from matplotlib import pyplot as plt
+from matplotlib import patches as mpatches
+import numpy as np 
+import parameters.VTOLParam as P
+# if you are having difficulty with the graphics, 
+# try using one of the following backends  
+# See https://matplotlib.org/stable/users/explain/backends.html
+# import matplotlib
+# matplotlib.use('qtagg')  # requires pyqt or pyside
+# matplotlib.use('ipympl')  # requires ipympl
+# matplotlib.use('gtk3agg')  # requires pyGObject and pycairo
+# matplotlib.use('gtk4agg')  # requires pyGObject and pycairo
+# matplotlib.use('gtk3cairo')  # requires pyGObject and pycairo
+# matplotlib.use('gtk4cairo')  # requires pyGObject and pycairo
+# matplotlib.use('tkagg')  # requires TkInter
+# matplotlib.use('wxagg')  # requires wxPython
+
+
+class VTOLAnimation:
+    def __init__(self, limits, multi):
+        # set up plot
+        self.fig = plt.figure(1)
+        if multi == True:
+            self.ax = self.fig.add_subplot(1, 2, 1)
+        else:
+            self.ax = self.fig.add_subplot(1, 1, 1)
+        # draw ground
+        plt.plot([-limits, limits], [0, 0])
+        # for list of objects
+        self.handle = []
+        # init flag
+        self.flag_init = True
+        # set limits
+        self.limits = limits
+
+    def update(self, state):
+        # # test
+        z = state[0][0]  # Horizontal position of cart, m
+        h = state[1][0]
+        theta = state[2][0]
+        
+        # correct
+        # z = state[0][0]  # Horizontal position of cart, m
+        # h = state[2][0]
+        # theta = state[4][0]
+
+        # draw plot elements: body, Lprop, Rprop
+        self.draw_body(z, h, theta)
+        self.draw_Lprop(z, h, theta)
+        self.draw_Rprop(z, h,theta)
+        self.ax.set_aspect('equal')
+        self.ax.set_ylim(bottom=-3,top=8)
+        self.ax.set_xlim(left=-6,right=6)
+        # Set initialization flag to False after first call
+        if self.flag_init == True:
+            self.flag_init = False
+    
+    def draw_body(self, z, h, theta):
+        # specify bottom left corner of rectangle
+        x = z-P.wb/2.0
+        y = h-P.hb/2
+        corner = (x, y)
+        # create rectangle on first call, update on subsequent calls
+        if self.flag_init is True:
+            # Create the Rectangle patch and append its handle
+            # to the handle list
+            self.handle.append(mpatches.Rectangle(corner, P.wb, P.hb, fc='blue', ec='black'))
+            # Add the patch to the axes
+            self.ax.add_patch(self.handle[0])
+        else:
+            self.handle[0].set_xy(corner)  # Update patch
+
+    def draw_Lprop(self, z, h, theta):
+        # specify bottom left corner of rectangle
+        x = [z-((P.wb/2)+(P.d*np.cos(-theta)))]
+        y = [h+(P.d*np.sin(-theta))]
+        center = (x, y)
+        # create circle on first call, update on subsequent calls
+        if self.flag_init is True:
+            # Create the Rectangle patch and append its handle
+            # to the handle list
+            self.handle.append(
+                mpatches.CirclePolygon(center, radius=0.05, resolution=15, fc='blue', ec='black'))
+            # Add the patch to the axes
+            self.ax.add_patch(self.handle[1])
+        else:
+            self.handle[1].xy=center  # Update patch
+    
+    def draw_Rprop(self, z, h, theta):
+       # specify bottom left corner of rectangle
+        x = [z+((P.wb/2)+(P.d*np.cos(-theta)))]
+        y = [h-(P.d*np.sin(-theta))]
+        center = (x, y)
+        # create circle on first call, update on subsequent calls
+        if self.flag_init is True:
+            # Create the Rectangle patch and append its handle
+            # to the handle list
+            self.handle.append(mpatches.CirclePolygon(center, radius=0.05, resolution=15, fc='blue', ec='black'))
+            # Add the patch to the axes
+            self.ax.add_patch(self.handle[2])
+        else:
+            self.handle[2].xy=center  # Update patch
+   
